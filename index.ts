c3b913915beacb9cdf86396386073eb1af42406b
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-03-30 17:59:53 -0300

    Types

--- index.ts
+++ index.ts
@@ -2,14 +2,24 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
+interface Result {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then((response) => {
-  const result = response.data;
+  const result = response.data as Result;
 
   const id = result.id;
   const title = result.title;
-  const fineshed = result.completed;
+  const completed = result.completed;
 
-  console.log(`
-    Todo  id: ${id}, title: ${title}... e fineshed: ${fineshed}
-    `);
+  logTodo(id, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    Todo  id: ${id}, title: ${title}... e fineshed: ${completed}
+    `);
+};
