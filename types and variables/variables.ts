87f629b5b250181f8df20dc60bd18455e691d246
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-04-01 21:28:32 -0300

    Object literal Annotations

--- types and variables/variables.ts
+++ types and variables/variables.ts
@@ -6,3 +6,21 @@ let nothingMuch: null = null;
 let nothing: undefined = undefined;
 
 //construído objetos
+
+let now: Date = new Date();
+
+//Array
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 16, 26];
+let trurhs: boolean[] = [true, false, false, true];
+
+//Classes
+class Car {}
+
+let car: Car = new Car();
+
+//Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
