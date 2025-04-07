470115e68e98811003dd572617dc0802ae8b71c0
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-04-06 23:28:30 -0300

    Exercios TypeScript

--- Exercícios/Nivel Facil/6-Enums.ts
+++ Exercícios/Nivel Facil/6-Enums.ts
@@ -23,4 +23,23 @@
 //   // sua lógica aqui
 // }
 
-enum 
+enum StatusPedido {
+  Pendente,
+  EmAndamento,
+  Concluido,
+}
+
+function mostrarStatusPedido(status: StatusPedido) {
+  if (status === StatusPedido.Pendente) {
+    console.log("Pedido pendente de aprovação");
+  }
+  if (status === StatusPedido.EmAndamento) {
+    console.log("Pedido está sendo processado");
+  } else if (status === StatusPedido.Concluido) {
+    console.log("Pedido finalizado com sucesso");
+  }
+}
+
+mostrarStatusPedido(StatusPedido.Pendente);
+mostrarStatusPedido(StatusPedido.EmAndamento);
+mostrarStatusPedido(StatusPedido.Concluido);
